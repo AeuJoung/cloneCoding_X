7109f9870e0c7986f800efabbe47d090633fc643
AEU <aeuplayground@gmail.com>
2024-04-17 19:47:19 +0900

    20240417

---
+++ src/app/(afterlogin)/layout.tsx
@@ -0,0 +1,14 @@
+export default function Layout({children} : {children : React.ReactNode}) {
+    return (
+        <main className=''>
+            <section>
+                <nav>
+                    
+                </nav>
+            </section>
+            {children}
+        </main>
+        
+    );
+}
+
