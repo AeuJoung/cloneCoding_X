6d250e2185cb67c15536e5bbdc832b80077175ac
AEU <aeuplayground@gmail.com>
2024-04-20 22:54:03 +0900

    20240420

--- src/app/(afterlogin)/layout.tsx
+++ src/app/(afterlogin)/layout.tsx
@@ -1,9 +1,18 @@
+import styles from './layout.module.css';
+
 export default function Layout({children} : {children : React.ReactNode}) {
     return (
         <main className=''>
             <section>
+                <div className={styles.logo}></div>
                 <nav>
-                    
+                    <ul>
+                        <li>Home</li>
+                        <li>Search</li>
+                        <li>Notice</li>
+                        <li>Message</li>
+                        <li>Message</li>
+                    </ul>
                 </nav>
             </section>
             {children}
